1f55079d9d88246750ef4a440783499511ed7702
Khushi <kaybesky89@gmail.com>
2024-08-04 14:46:54 +0530

    frontend url pasted

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 //Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: 'https://todofrontend-dun.vercel.app', // Your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
